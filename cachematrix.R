dca62677016bf8619d4410151bb10209c0bb896d
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invM <- NULL
+  setM <- function(y){
+    x <<- y
+    invM <<- NULL
+  }
+  getM <- function() x
+  setIM <- function(inv) invM <<- inv
+  getIM <- function() invM
+  list(setIM = setIM, getM = getM, setIM = setIM, getIM = getIM)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invM <- x$getIM()
+  if(!is.null(invM)){
+    message("get cache martix")
+    return(invM)
+  }
+  data <- x$getM()
+  invM <- solve(data)
+  x$setIM(invM)
+  inv      
 }
+
+
+
+
+
